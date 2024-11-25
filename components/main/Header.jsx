c644a7c57c4baa0ea8aa02fa1a9962a472c71b6e
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2024-11-25 12:17:36 +0530

    created my portfolio repository

---
+++ components/main/Header.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Button } from "../ui/Button";
+
+//Nav component
+import Nav from "../main/Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+
+        {/* logo */}
+        <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Chamara<span className="text-accent">.</span>
+            </h1>
+        </Link>
+
+        {/* desktop navigation & hire me button */}
+        <div className="hidden xl:flex item-center gap-8">
+          <Nav/>
+            <Link href="/contact">
+              <Button>Hire Me</Button>
+            </Link>
+        </div>
+
+
+        {/* mobile navigation */}
+        <div className="xl:hidden">
+          Mobile Nav
+        </div>
+      
+      </div>
+    </header>
+  );
+};
+
+export default Header;s
---
+++ components/main/Nav.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from 'next/link';
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "Services",
+        path: "/Services",
+    },
+    {
+        name: "Rseume",
+        path: "/Rseume",
+    },
+    {
+        name: "Works",
+        path: "/Works",
+    },
+    {
+        name: "Contact",
+        path: "/Contact",
+    },
+]
+
+const Nav = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+
+    return (
+        <nav className="flex gap-8">
+            {links.map((link, index) => {
+                return (
+                    <Link
+                        href={link.path}
+                        key={index}
+                        className={`${link.path === pathname && "text-accent border-b-2 border-accent"
+                            } capitalize font-medium hover:text-accent-hover transition-all`}
+                    >
+                        {link.name}
+                    </Link>
+                );
+            })}
+        </nav>
+    );
+};
+
+export default Nav;
