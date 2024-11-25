c141471c9b04be81db7d6194275a6d9f9c347e99
scssandanayake <114943338+scssandanayake@users.noreply.github.com>
2024-11-25 12:24:31 +0530

    test2

--- components/main/Header.jsx
+++ components/main/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
             <h1 className="text-4xl font-semibold">
-              Chamara<span className="text-accent">.</span>
+              CS<span className="text-accent">.</span>
             </h1>
         </Link>
 
